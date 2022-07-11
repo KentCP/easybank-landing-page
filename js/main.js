6267d30358b94c998fd4e4efc8cba19c41ffe93e
KentCP <pkchunga24@gmail.com>
2022-07-11 10:08:28 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+const navbar = document.querySelector('.navbar');
+const openMenu = document.querySelector('.open__menu');
+const closeMenu = document.querySelector('.close__menu');
+
+openMenu.addEventListener('click', () => {
+
+	navbar.classList().add('.open__menu')
+
+});
